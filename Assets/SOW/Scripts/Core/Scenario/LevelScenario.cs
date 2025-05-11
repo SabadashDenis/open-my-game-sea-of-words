b20a7bdfe16561219314ecbb100f2cdc590a9657
Denis Sabadash <denissabadash909@gmail.com>
2025-05-11 16:50:21 +0300

    up: get common letters for words as utility

--- Assets/SOW/Scripts/Core/Scenario/LevelScenario.cs
+++ Assets/SOW/Scripts/Core/Scenario/LevelScenario.cs
@@ -5,6 +5,7 @@ using Cysharp.Threading.Tasks;
 using SoW.Scripts.Core.Configs;
 using SoW.Scripts.Core.Scenario._;
 using SoW.Scripts.Core.UI.Screen.Game.Views;
+using SoW.Scripts.Core.Utility;
 
 namespace SoW.Scripts.Core.Scenario
 {
@@ -28,16 +29,16 @@ namespace SoW.Scripts.Core.Scenario
             var levelsData = Data.Config.Levels.Data.data;
             _levelData = levelsData[Preset.LevelIndex % levelsData.Count];
             
-            GetLettersChain(_levelData.words);
-            
             _gameScreen.Show();
+            
+            _gameScreen.SetLevel(Preset.RealLevelNumber);
             
             var bestLettersSize = await _gameScreen.GetBestLettersFitSize(_levelData.words, Preset.MaxGridsCount);
             var lengthSortedWords = _levelData.words.OrderBy(x => x.Length).ToArray();
-            
-            _gameScreen.SetLevel(Preset.RealLevelNumber);
             _gameScreen.SetupWords(lengthSortedWords, bestLettersSize);
-            _gameScreen.InputCircle.SetupLetters(GetLettersChain(_levelData.words));
+            
+            var letters = WordsUtility.GetCommonLetters(_levelData.words);
+            _gameScreen.InputCircle.SetupLetters(letters);
             
             Data.Input.Click.Released += ProcessInputFinish;
             
@@ -47,49 +48,6 @@ namespace SoW.Scripts.Core.Scenario
                 inputLetterView.Tap += OnLetterTap;
             }
             await UniTask.WaitUntil(() => _foundedWords.Count == _levelData.words.Length, cancellationToken: token);
-        }
-
-        private string GetLettersChain(string[] words)
-        {
-            Dictionary<char, int> result = new();
-            
-            foreach (string word in words)
-            {
-                Dictionary<char, int> wordLetters = new();
-                
-                foreach (char letter in word)
-                {
-                    if (!wordLetters.TryAdd(letter, 1))
-                    {
-                        wordLetters[letter]++;
-                    }
-                }
-
-                foreach (var wordLetter in wordLetters)
-                {
-                    if (result.TryGetValue(wordLetter.Key, out var resultLettersCount))
-                    {
-                        if(resultLettersCount < wordLetter.Value)
-                            result[wordLetter.Key] = wordLetter.Value;
-                    }
-                    else
-                    {
-                        result.Add(wordLetter.Key, wordLetter.Value);
-                    }
-                }
-            }
-
-            string lettersChain = string.Empty;
-
-            foreach (var letterData in result)
-            {
-                for (int i = 0; i < letterData.Value; i++)
-                {
-                    lettersChain += letterData.Key;
-                }
-            }
-            
-            return lettersChain;
         }
 
         private void OnLetterHover(SelectableLetterView letterView)
---
+++ Assets/SOW/Scripts/Core/Utility/WordsUtility.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace SoW.Scripts.Core.Utility
+{
+    public static class WordsUtility
+    {
+        public static string GetCommonLetters(string[] words)
+        {
+            Dictionary<char, int> result = new();
+            
+            foreach (string word in words)
+            {
+                Dictionary<char, int> wordLetters = new();
+                
+                foreach (char letter in word)
+                {
+                    if (!wordLetters.TryAdd(letter, 1))
+                    {
+                        wordLetters[letter]++;
+                    }
+                }
+
+                foreach (var wordLetter in wordLetters)
+                {
+                    if (result.TryGetValue(wordLetter.Key, out var resultLettersCount))
+                    {
+                        if(resultLettersCount < wordLetter.Value)
+                            result[wordLetter.Key] = wordLetter.Value;
+                    }
+                    else
+                    {
+                        result.Add(wordLetter.Key, wordLetter.Value);
+                    }
+                }
+            }
+
+            string lettersChain = string.Empty;
+
+            foreach (var letterData in result)
+            {
+                for (int i = 0; i < letterData.Value; i++)
+                {
+                    lettersChain += letterData.Key;
+                }
+            }
+            
+            return lettersChain;
+        }
+    }
+}
